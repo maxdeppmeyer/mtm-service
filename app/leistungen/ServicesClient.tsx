1ce4c61ef05a82a1b61a565461c589ac2daa2847
maxdeppmeyer <max.deppmeyer.prozesse@gmail.com>
2026-05-29 00:24:58 +0200

    v12

--- app/leistungen/ServicesClient.tsx
+++ app/leistungen/ServicesClient.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
 import { ArrowRight, CheckCircle2 } from "lucide-react";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
